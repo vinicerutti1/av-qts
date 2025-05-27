7a66f8189005944c6e84c23d23c030836d884610
Kétly Machado <ketly.machado@gmail.com>
2025-05-26 23:01:10 -0300

    test: add task and auth integration tests

---
+++ server/src/tests/routes/auth.routes.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest';
+import app from '../../app';
+import { prisma } from '../../utils/prisma';
+import { setupTestDB, disconnectTestDB } from '../setup.test.db';
+
+beforeAll(async () => {
+    await setupTestDB();
+});
+
+afterAll(async () => {
+    await disconnectTestDB();
+});
+
+describe('AuthController', () => {
+    describe('POST /api/auth/register', () => {
+        it('deve registrar usuário com dados válidos', async () => {
+            // Arrange (preparar)
+            const userData = {
+                email: 'usuario@valido.teste',
+                password: 'senha',
+                name: 'Usuário Válido',
+            };
+
+            // Act (agir)
+            const response = await request(app).post('/api/auth/register').send(userData);
+
+            // Assert (verificar)
+            expect(response.status).toBe(201);
+            expect(response.body).toMatchObject({
+                user: {
+                    id: expect.any(Number),
+                    email: userData.email,
+                    name: userData.name,
+                },
+                token: expect.any(String),
+            });
+
+            const userInDB = await prisma.user.findUnique({ where: { email: userData.email } });
+            expect(userInDB).not.toBeNull();
+            expect(userInDB?.email).toBe(userData.email);
+            expect(userInDB?.password).not.toBe(userData.password);
+        });
+    });
+});
---
+++ server/src/tests/routes/task.routes.test.ts
@@ -0,0 +1,61 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+// Mock do middleware de autenticação para simular a autenticação durante o teste
+jest.mock('../../middlewares/auth.middleware', () => ({
+    authenticate: (req: any, res: any, next: any) => {
+        req.userId = 1;
+        next();
+    },
+}));
+
+import request from 'supertest';
+import app from '../../app';
+import { prisma } from '../../utils/prisma';
+import { setupTestDB, disconnectTestDB } from '../setup.test.db';
+
+beforeAll(async () => {
+    await setupTestDB();
+});
+
+afterAll(async () => {
+    await disconnectTestDB();
+});
+
+describe('TaskController', () => {
+    describe('POST /api/tasks', () => {
+        it('deve criar tarefa com dados válidos', async () => {
+            // Arrange (preparar)
+            const taskData = {
+                title: `Tarefa válida ${new Date()}`,
+                description: 'Essa é uma tarefa válida',
+                completed: false,
+                priority: 'low',
+            };
+
+            // Act (agir)
+            const response = await request(app).post('/api/tasks').send(taskData);
+
+            // Assert (verificar)
+            expect(response.statusCode).toBe(201);
+            expect(response.body).toEqual({
+                ...taskData,
+                id: expect.any(Number),
+                userId: 1,
+                dueDate: null,
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+            });
+
+            const taskInDB = await prisma.task.findFirst({ where: { title: taskData.title } });
+            expect(taskInDB).toEqual(
+                expect.objectContaining({
+                    ...taskData,
+                    id: expect.any(Number),
+                    userId: 1,
+                    createdAt: expect.any(Date),
+                    updatedAt: expect.any(Date),
+                }),
+            );
+        });
+    });
+});
