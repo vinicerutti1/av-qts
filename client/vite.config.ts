fcf03c0ecdb6a13bc474e7284421ede2c21fb34f
Kétly Machado <ketly.machado@gmail.com>
2025-05-26 23:17:49 -0300

    chore: frontend unit test setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import * as dotenv from 'dotenv';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 dotenv.config();
 
@@ -11,5 +11,12 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     define: {
         'import.meta.env.VITE_API_URL': JSON.stringify(API_URL),
+    },
+    test: {
+        environment: 'jsdom',
+        coverage: {
+            reporter: ['text', 'html'],
+        },
+        globals: true,
     },
 });
