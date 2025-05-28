aae2a1b8758a0c221a0812d9fb6cb7acf5beb6a1
Kétly Machado <ketly.machado@gmail.com>
2025-05-28 20:29:40 -0300

    test: reset sequence

--- server/src/tests/setup.test.db.ts
+++ server/src/tests/setup.test.db.ts
@@ -2,18 +2,23 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+export let testUser: { id: number };
+
 export const setupTestDB = async () => {
     await prisma.task.deleteMany();
     await prisma.user.deleteMany();
+    await prisma.$executeRawUnsafe('DELETE FROM sqlite_sequence WHERE name = "User";');
+    await prisma.$executeRawUnsafe('DELETE FROM sqlite_sequence WHERE name = "Task";');
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
         data: {
-            id: 1,
             email: 'usuario@exemplo.teste',
             name: 'Usuário Exemplo',
             password: 'senha',
         },
     });
+
+    testUser = { id: user.id };
 };
 
 export const disconnectTestDB = async () => {
